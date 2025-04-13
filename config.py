45631b03ea6f35ba93e8f980aa948eea1c7dfdea
TsuNamiStudio <xtsunamistudio@gmail.com>
2025-04-13 07:36:17 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+INFURA_URL = "https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID"
+PRIVATE_KEY = "your_private_key"
+WALLET_ADDRESS = "0xYourWalletAddress"
+CHAIN_ID = 11155111  # Sepolia
+GAS = 2000000
+GAS_PRICE_GWEI = 30
